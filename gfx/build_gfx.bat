5f0b0dcfc4e7cb078180246e5d33b445640af33e
hlide <hlide@free.fr>
2021-10-10 20:32:06 +0200

    Stronger DRAM failure detection and ability to add several screen pages and tests.

--- gfx/build_gfx.bat
+++ gfx/build_gfx.bat
@@ -1,2 +1,5 @@
-mzdrawgen.exe DRAMTester
+mzdrawgen.exe --binary --no-color DRAMTester_screen0
+mzdrawgen.exe --binary --no-color DRAMTester_screen1
+zx0 -f DRAMTester_screen0.bin DRAMTester_screen0.zx0
+zx0 -f DRAMTester_screen1.bin DRAMTester_screen1.zx0
 pause
