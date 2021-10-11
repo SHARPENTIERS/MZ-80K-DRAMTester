770e34418ea4e367618af9dbb14b2d1296489d9c
hlide <hlide@free.fr>
2021-10-11 02:54:45 +0200

    Prepare for more DRAM tests.

--- gfx/build_gfx.bat
+++ gfx/build_gfx.bat
@@ -1,5 +1,7 @@
-mzdrawgen.exe --binary --no-color DRAMTester_screen0
-mzdrawgen.exe --binary --no-color DRAMTester_screen1
-zx0 -f DRAMTester_screen0.bin DRAMTester_screen0.zx0
-zx0 -f DRAMTester_screen1.bin DRAMTester_screen1.zx0
+mzdrawgen.exe --binary --no-color DRAMTester_screen0.0
+mzdrawgen.exe --binary --no-color DRAMTester_screen0.1
+mzdrawgen.exe --binary --no-color DRAMTester_screen1.0
+zx0 -f DRAMTester_screen0.0.bin DRAMTester_screen0.0.zx0
+zx0 -f DRAMTester_screen0.1.bin DRAMTester_screen0.1.zx0
+zx0 -f DRAMTester_screen1.0.bin DRAMTester_screen1.0.zx0
 pause
